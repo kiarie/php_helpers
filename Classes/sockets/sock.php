65c9e0bf332551c73d43f4b5d9f1419520310210
kiarie Mburu <kiarie@ipayafrica.com>
2017-08-24 11:28:17 +0300

    Edits to the socket class which have the namespace included and allow for usage in a namespaced environment

--- Classes/sockets/sock.php
+++ Classes/sockets/sock.php
@@ -1,7 +1,5 @@
 <?php
 namespace elipa\sockets;
-error_reporting(E_ALL); 
-ini_set("display_errors", 1);
 class Sock{
     
     function __construct($url, $port= 80, $persistent = false)
@@ -10,96 +8,11 @@ class Sock{
         $this->original_url =  $url;
         $this->url = (parse_url($url, PHP_URL_QUERY))? parse_url($url): parse_url($url);
         }else{
-            throw new InvalidArgumentException("Invalid URL given " .$url);
+            throw new \InvalidArgumentException("Invalid URL given " .$url);
         }
         $this->fp = ($persistent)? fsockopen($this->url['host'], $port, $errno, $errstr, 30):pfsockopen($this->url['host'], $port, $errno, $errstr, 30);//pfsockopen() is for persistent connections 
         if(!$this->fp){
-            throw new Exception("Error Processing Socket Request".$errno." - ".$errstr, 1);         
-        }
-    }
-    private function build_params($data)
-    {
-        return (is_array($data))?http_build_query($data):'';
-    }
-    /*
-    *   @function puter writes to the socket rather puts to it, similar to the writer function below
-    **/
-    function puter($string)
-    { 
-        return (empty($string))?'': fputs($this->fp, $string);
-    }
-    /**
-    * @function writer writes to the socket it can be called on many times and is used internally by this class
-    */
-    function writer($string)
-    { 
-    	return (empty($string))?'': fwrite($this->fp, $string);
-    }
-    /**
-    **** @function method, takes in method e.g. GET, [content_type,  content] these two especially needed for post
-    *      but are optional therefore not needed most of the time, keepalive also for the keepalive header and protocol in case 
-    *      you are accessing another protocol default is HTTP/1.1 
-    */
-    function method($method, $content="", $content_type="www-form", $keepalive = true, $protocol = 'HTTP/1.1')
-    {
-        //Get the path if any default to / if not 
-        $this->path = (isset($this->url['path']))?$this->url['path']:'/';
-        //Get the query params if any useful for GET
-        $query = (isset($this->url['query']))? "?".http_build_query($this->url['query']):'';
-        //set encoding for now just gzip
-        $this->encoding = (substr_compare($protocol, 'HTTP', 0, 4) === 0)? "Accept-Encoding: gzip, deflate, br\r\n" :'';
-        //connection header with keep-alive and close as options
-        $this->keepalive = ($keepalive)?"Connection: keep-alive\r\n":"Connection: Close\r\n";
-        //content for post maybe get?
-        $this->content = $this->build_params($content);
-        // exit(var_dump($this->content));
-        switch ($method) {
-            case 'POST':
-                    $this->post($this->content, $content_type);
-                break;
-            case 'GET':
-                    $this->get($query);
-                break;
-            default:
-                throw new Exception($method." Not a Supported Method ", 1);
-                break;
-        }
-    }
-    function get($content, $content_type)
-    {
-            $writer = "GET ".$this->path.$content." HTTP/1.1\r\n";
-            $writer .= "Host: ".$this->url['host']." \r\n";
-            $writer .= $this->encoding;
-            $writer .= "Origin: http://localhost\r\n";
-            $writer .= $this->content_type($content_type);
-            $writer .= $this->keepalive;//this sure helped
-            $writer .= "\r\n";
-            $this->writer($writer);
-    }
-    function post($content, $content_type)
-    {
-            $writer = "POST ".$this->url['path']." HTTP/1.1\r\n";
-            $writer .= "Host: ".$this->url['host']." \r\n";
-            $writer .= $this->encoding;
-            // $writer .= "Origin: ".$this->url['scheme']."://".$this->url['host']."\r\n";
-            $writer .= $this->content_type($content_type);
-            $writer .= "Content-Length: ".strlen($content)."\r\n";
-            $writer .= $this->keepalive;//this sure helpd
-            $writer .= "\r\n";
-            $writer .= $content; //the POST content here
-            $this->writeclass Sock{
-    
-    function __construct($url, $port= 80, $persistent = false)
-    {
-        if(parse_url($url)){
-        $this->original_url =  $url;
-        $this->url = (parse_url($url, PHP_URL_QUERY))? parse_url($url): parse_url($url);
-        }else{
-            throw new InvalidArgumentException("Invalid URL given " .$url);
-        }
-        $this->fp = ($persistent)? fsockopen($this->url['host'], $port, $errno, $errstr, 30):pfsockopen($this->url['host'], $port, $errno, $errstr, 30);//pfsockopen() is for persistent connections 
-        if(!$this->fp){
-            throw new Exception("Error Processing Socket Request".$errno." - ".$errstr, 1);         
+            throw new \Exception("Error Processing Socket Request".$errno." - ".$errstr, 1);         
         }
     }
     private function build_params($data)
@@ -146,7 +59,7 @@ class Sock{
                     $this->get($query);
                 break;
             default:
-                throw new Exception($method." Not a Supported Method ", 1);
+                throw new \Exception($method." Not a Supported Method ", 1);
                 break;
         }
     }
@@ -158,7 +71,6 @@ class Sock{
             $writer = "GET ".$this->path.$content." HTTP/1.1\r\n";
             $writer .= "Host: ".$this->url['host']." \r\n";
             $writer .= $this->encoding;
-            $writer .= "Origin: http://localhost\r\n";
             $writer .= $this->content_type($content_type);
             $writer .= $this->keepalive;//this sure helped
             $writer .= "\r\n";
@@ -172,7 +84,6 @@ class Sock{
             $writer = "POST ".$this->url['path']." HTTP/1.1\r\n";
             $writer .= "Host: ".$this->url['host']." \r\n";
             $writer .= $this->encoding;
-            // $writer .= "Origin: ".$this->url['scheme']."://".$this->url['host']."\r\n";
             $writer .= $this->content_type($content_type);
             $writer .= "Content-Length: ".strlen($content)."\r\n";
             $writer .= $this->keepalive;//this sure helpd
@@ -210,5 +121,4 @@ class Sock{
     {
         fclose($this->fp);
     }
-}
-?>
+}?>
