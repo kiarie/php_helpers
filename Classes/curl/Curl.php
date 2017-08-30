eda6b7fb6e246bce04e23886acef4e41d28ffc0f
Starford <starford@gmail.com>
2017-08-30 16:05:01 +0300

    added the init curl class with GET and POST methods, more functionality to be pushed later

---
+++ Classes/curl/Curl.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * eLipa Curl (https://ipayafrica.com)
+ *
+ * @link      https://ipayafrica.com
+ * @copyright Copyright (c) 2014-2017 Starford Gwala
+ */
+namespace elipa\curl;
+Class Curl 
+{
+    const DEFAULT_TIMEOUT = 30;
+    public $curl;
+	
+
+/*
+|--------------------------------------------------------------------------
+| CURL
+|--------------------------------------------------------------------------
+|
+| This creates a new instance of the class
+|
+*/
+
+	function __construct()
+	{
+        if (!extension_loaded('curl')) {
+            throw new \ErrorException('cURL library is not loaded');
+        }
+        
+        $this->curl = curl_init();
+	}
+
+/*
+|--------------------------------------------------------------------------
+| get
+|--------------------------------------------------------------------------
+|
+| accepts @url and @arguments
+| @url is a url
+| @arguments is an array
+| returns curl response
+|
+*/
+	public function get($url, $arguments = array())
+	{
+			if(!empty($arguments)):
+					$url = $url.'?'.http_build_query($arguments);
+			endif;
+			curl_setopt( $this->curl , CURLOPT_URL, $url);
+			curl_setopt($this->curl , CURLOPT_CONNECTTIMEOUT, 30);
+			curl_setopt($this->curl, CURLOPT_TIMEOUT, 3);
+			// grab URL and pass it to the browser
+			$response = curl_exec($this->curl);
+			// close cURL resource, and free up system resources
+			curl_close($this->curl);
+			return $response;
+			
+
+	}
+
+/*
+|--------------------------------------------------------------------------
+| get
+|--------------------------------------------------------------------------
+|
+| accepts @url and @arguments
+| @url is a url
+| @arguments is an array
+| returns curl response
+|
+*/
+	public function post($url, $arguments = array())
+	{
+			if(!empty($arguments)):
+				throw new \ErrorException('Missing POST ARGUMENTS');
+			endif;
+			$ch = curl_init($url);
+            curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($arguments));
+            curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, 3);
+  			curl_setopt($this->curl, CURLOPT_TIMEOUT, 3);
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, array( "content-type: application/x-www-form-urlencoded")); 
+
+			$result = curl_exec($this->curl);
+			// close cURL resource, and free up system resources
+			curl_close($this->curl);
+            return $result;
+
+	}
+	
+}
+?>
